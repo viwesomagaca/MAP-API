65a5588fe47339904c87c9e59b1e16a9942b5a94
viwesomagaca <viwesomagaca29@gmail.com>
2018-01-24 10:51:38 +0200

    Made an ajax call to get the schools api

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//AIzaSyBbDhDA5ekyjelELNvW3qngMevQY2gFEcg
+$(document).ready(function(){
+
+  $(".search").click(function(){
+  $.ajax({
+    url:"https://developer.nrel.gov/api/windexchange/schoolprojects?api_key=BpwET3I8qcPGHgBcgcECMNuYXfDVEz3zwKN00w1f",
+    type:"GET"
+  }).then(function(data){
+    console.log(data);
+  })
+
+});
+});
