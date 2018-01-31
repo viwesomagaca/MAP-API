250c4e283dc31b8cf1f761ec2c81121f22e9b92c
viwesomagaca <viwesomagaca29@gmail.com>
2018-01-31 16:03:07 +0200

    Fixed Link search

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {                                                  //Populate the document and tell it to be ready
-
   var cityData;                                                                 //define the variable
-
   var availableSchools = document.getElementById('projects').innerHTML;         //compile handlebars templates
   var template = Handlebars.compile(availableSchools);
   var foundProject = document.getElementById('output').innerHTML;
   var template2 = Handlebars.compile(foundProject);
-
-
 
   $.ajax({                                                                       //ajax call that retrieves data from the API
     url: "https://developer.nrel.gov/api/windexchange/schoolprojects?api_key=BpwET3I8qcPGHgBcgcECMNuYXfDVEz3zwKN00w1f",
@@ -17,17 +13,22 @@ $(document).ready(function() {                                                  
       school: data
     });
     cityData = data;
-    var map1 = myMap(51.508742, -0.120850,3)                                     //Renders the map when the page loads
 
-    data.forEach(function(result) {                                             //loop through data from API and call marker function,
-      myMarker(result.Latitude, result.Longitude, map1);                        //pass the results and values from the marker function parameters in order to render them for the first time
+    var map1 = myMap(cityData[0].Latitude, cityData[0].Longitude,3) //Renders the map when the page loads
+
+    data.forEach(function(result) { //loop through data from API and call marker function,
+      myMarker(result.Latitude, result.Longitude, map1); //pass the results and values from the marker function parameters in order to render them for the first time
     });
+
+    cityData.forEach(function(myCity){
+      $('#schoolitem'+ myCity.ID).click(function(schoolName){
+          var map1 = myMap(myCity.Latitude, myCity.Longitude,15);
+        myMarker(myCity.Latitude, myCity.Longitude, map1);
+      })
+    })
   });
 
-
-  function myMap(Latitude, Longitude, zoom) {
-                                                                                //Renders the map on the browser
-
+  function myMap(Latitude, Longitude, zoom) {   //Renders the map on the browser                                                                
     var mapOptions1 = {
       center: new google.maps.LatLng(Latitude, Longitude),
       zoom: zoom,
@@ -65,18 +66,17 @@ $(document).ready(function() {                                                  
 
     } else if (myCity === undefined) {                                          //Displays error message when the city is not found
       document.getElementById('message').innerHTML = "City is not found";
-
-    }else{
-      document.getElementById('message').innerHTML = "";                        //It clears the text box after the undefined city is written
     }
+      document.getElementById('message').innerHTML = "";
+
     var myCity = cityData.filter(function(item) {                               //Filters the according to cities available on the data
       return item.City.toLowerCase().trim() === searchCity.toLowerCase().trim(); //returns the cities found and trim to remove white spaces
     })
     document.querySelector(".findMe").innerHTML = template2({                     //Targets the template and populate it to display the address of the city searched
       myCity: myCity
     });
-   searchCity.innerHTML="";
-    var map1 = myMap(0, 0, 2);                                                   //Define variable to carry map function the starts of longitude and latitude as well as pass zoom number.
+
+    var map1 = myMap(myCity[0].Latitude,myCity[0].Longitude, 10);                                                   //Define variable to carry map function the starts of longitude and latitude as well as pass zoom number.
 
     myCity.forEach(function(city) {                                              //Looping through data from filtering and renders markers in all the education projects/institutions found
       myMarker(city.Latitude, city.Longitude, map1);
